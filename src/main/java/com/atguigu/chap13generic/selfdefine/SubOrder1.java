92144e547f67900330749dd790f01d96cdffca87
ylqi007 <ylqi92121@gmail.com>
2024-03-24 10:33:26 -0700

    范型: 自定义范型类/范型接口, 范型方法

---
+++ src/main/java/com/atguigu/chap13generic/selfdefine/SubOrder1.java
@@ -0,0 +1,10 @@
+package com.atguigu.chap13generic.selfdefine;
+
+/**
+ * Description: SubOrder1不是范型类
+ *
+ * @Author: ylqi007
+ * @Create: 3/24/24 09:12
+ */
+public class SubOrder1 extends Order{
+}
---
+++ src/main/java/com/atguigu/chap13generic/selfdefine/SubOrder5.java
@@ -0,0 +1,25 @@
+package com.atguigu.chap13generic.selfdefine;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Description: SubOrder5是范型类
+ *
+ * @Author: ylqi007
+ * @Create: 3/24/24 09:25
+ */
+@Getter
+@Setter
+public class SubOrder5<T, E> extends Order<T> {
+    E e;
+
+    public SubOrder5() {
+    }
+
+    // 此时，构造器中有两个范型类型: T 和 E
+    public SubOrder5(T t, int orderId, E e) {
+        super(t, orderId);
+        this.e = e;
+    }
+}
